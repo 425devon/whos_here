dad416aec2b1e0c5f91c484af76c2e40d695cd08
Chase <c.coalwell@gmail.com>
2018-03-26 11:14:30 -0700

    initial commit

---
+++ src/main/java/com/dojo/whoshere/WhosHereApplication.java
@@ -0,0 +1,12 @@
+package com.dojo.whoshere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhosHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhosHereApplication.class, args);
+	}
+}
