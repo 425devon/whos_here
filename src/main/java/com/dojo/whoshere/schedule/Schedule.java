0c47373190e87f21d6dd2cadf3761e08808f799d
Devon Langendoerfer <425devon@gmail.com>
2018-03-26 15:46:28 -0700

    added cli call to arp-scan & data parsing of the results

--- src/main/java/com/dojo/whoshere/schedule/Schedule.java
+++ src/main/java/com/dojo/whoshere/schedule/Schedule.java
@@ -1,5 +1,10 @@
 package com.dojo.whoshere.schedule;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,5 +13,42 @@ public class Schedule {
 	@Scheduled(cron = "0 * * * * ?")
 	public void scheduledTask() {
 		System.out.println("scheduled job is starting");
+		try {
+			Process p = Runtime.getRuntime().exec("sudo arp-scan --localnet");
+			
+			BufferedReader stdInput = new BufferedReader(new 
+				     InputStreamReader(p.getInputStream()));
+			
+			
+			// read the output from the command and add to a list
+			System.out.println("Here is the standard output of the command:\n");
+			String s = null;
+			ArrayList<String> parsedData = new ArrayList<String>();
+			while ((s = stdInput.readLine()) != null) {
+			    parsedData.add(s);
+			}
+			
+			//call dataCleaner to remove header and footer statements for arp scanner
+			List<String> cleanedData = dataCleaner(parsedData);
+			System.out.println(cleanedData);
+			
+		} catch (IOException e) {
+			System.out.println("this was not the cli call you were looking for...");
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static List<String> dataCleaner(List<String> data) {
+		ArrayList<String> cleanData = (ArrayList<String>) data;
+		
+		for(int i = 0; i<3; i++) {
+			cleanData.remove(0);
+		}
+		for(int n = 0; n < 5; n++) {
+			cleanData.remove(cleanData.size()-1);
+		}
+		
+		return cleanData;
 	}
 }
